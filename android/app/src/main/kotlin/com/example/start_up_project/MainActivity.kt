0b10824b9f324003395b5d2c11061412a47ae1cc
Sanjeevi <62133659+sanjeevi12@users.noreply.github.com>
2021-03-18 13:33:26 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/start_up_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.start_up_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/constants/strings.dart
@@ -0,0 +1,10 @@
+class Strings{
+  //App
+  static final appTitle = "Sample App";
+
+  
+  // HomeScreen
+  static final homeScreenTitle = 'Home Screen';
+  static final homeScreenCenterText = 'Welcome to Flutterly!';
+
+}
